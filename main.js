079e0719fa436e8c91b4f997a053178ac6314594
kgni <admin@mdia.dk>
2022-05-01 19:22:34 +0200

    add functionality to delete item from the list

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 // Take input and insert it into DOM
 
-const list = [];
+// const list = [];
 
 const inputBtn = document.querySelector('.input-btn');
 const ul = document.querySelector('ul');
 
-const deleteBtn = document.querySelectorAll('.delete-btn');
+const deleteBtns = document.querySelectorAll('.delete-btn');
 
 inputBtn.addEventListener('click', addTodoItem);
-// deleteBtn.addEventListener('click', deleteTodoItem);
 
 function addTodoItem() {
 	const input = document.querySelector('input');
@@ -18,8 +17,10 @@ function addTodoItem() {
 		// Creating LI and appending to UL
 		let li = document.createElement('li');
 		li.innerText = input.value;
+		// list.push(li);
+		// const listItemIndex = `index${list.length - 1}`;
+		// li.classList.add(listItemIndex);
 		ul.appendChild(li);
-		list.push(li);
 
 		// Creating buttons and appending it to li
 		let buttons = document.createElement('div');
@@ -28,21 +29,30 @@ function addTodoItem() {
 		li.appendChild(buttons);
 
 		// Creating buttons and appending them to the buttons div
+
+		// Edit Button:
 		let editBtn = document.createElement('i');
 		editBtn.classList.add('fa-solid', 'fa-pen-to-square', 'edit-btn');
 		buttons.appendChild(editBtn);
 
+		// Delete Button:
+
 		let deleteBtn = document.createElement('i');
 		deleteBtn.classList.add('fa-solid', 'fa-x', 'delete-btn');
+		deleteBtn.addEventListener('click', deleteTodoItem);
 		buttons.appendChild(deleteBtn);
 
+		// console.log(
+		// 	`Added ${input.value} to the list. This is the ${list.length} item`
+		// );
 		input.value = '';
 	}
 }
 
-// function deleteTodoItem(e) {
-// 	e.target.removeChild(li);
-// }
+function deleteTodoItem(e) {
+	// const li = e.target.parentElement.parentElement;
+	// console.log(li);
+	e.target.parentElement.parentElement.remove();
+}
 
-// Create button for editing and deleting array item
-// add it all to local storage
+// TODO - add it all to local storage
